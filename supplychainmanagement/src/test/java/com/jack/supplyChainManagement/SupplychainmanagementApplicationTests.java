6e5f1d83eb9327e78163118885d6a5f5338206ef
jackdeng <Jack.z.deng@outlook.com>
2021-04-30 16:08:44 +0800

    first commit

---
+++ supplychainmanagement/src/test/java/com/jack/supplyChainManagement/SupplychainmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jack.supplyChainManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplychainmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
