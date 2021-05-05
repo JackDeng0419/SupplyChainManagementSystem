5e8d77ea08e3e91ba16ab2089efad39f2a56ac09
jackdeng <Jack.z.deng@outlook.com>
2021-05-05 22:44:02 +0800

    test commit

---
+++ supplychainmanagement/scm-admin/src/main/java/com/jack/admin/model/CaptchaImageModel.java
@@ -0,0 +1,29 @@
+package com.jack.admin.model;
+
+import java.time.LocalDateTime;
+
+
+public class CaptchaImageModel {
+
+    private String code;
+
+    private LocalDateTime expireTime;
+
+
+    public CaptchaImageModel(String code, int expireAfterSeconds){
+        this.code = code;
+        this.expireTime = LocalDateTime.now().plusSeconds(expireAfterSeconds);
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    /**
+     * 验证码是否失效
+     * @return
+     */
+    public boolean isExpired(){
+        return  LocalDateTime.now().isAfter(expireTime);
+    }
+}
